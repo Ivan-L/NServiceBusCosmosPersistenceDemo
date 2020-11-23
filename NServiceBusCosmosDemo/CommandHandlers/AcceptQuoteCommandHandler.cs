11482e54300d32d1beedac7ea9ac3aa0e06198ea
Ivan Lazarov <lazarovi@out.co.za>
2020-11-24 01:34:18 +0200

    Used v2 repository

--- NServiceBusCosmosDemo/CommandHandlers/AcceptQuoteCommandHandler.cs
+++ NServiceBusCosmosDemo/CommandHandlers/AcceptQuoteCommandHandler.cs
@@ -11,23 +11,21 @@ namespace NServiceBusCosmosDemo.CommandHandlers
 {
     public class AcceptQuoteCommandHandler : IHandleMessages<AcceptQuoteCommand>
     {
-        private readonly IRepositoryV1<Quote> _repository;
+        private readonly IRepositoryV2<Quote> _repository;
 
-        public AcceptQuoteCommandHandler(IRepositoryV1<Quote> repository)
+        public AcceptQuoteCommandHandler(IRepositoryV2<Quote> repository)
         {
             _repository = repository;
         }
 
         public async Task Handle(AcceptQuoteCommand message, IMessageHandlerContext context)
         {
-            var quote = await _repository.Load(message.QuoteNumber, new PartitionKey(message.QuoteNumber))
+            var quote = await _repository.Load(message.QuoteNumber)
                 .ConfigureAwait(false);
             
             quote.Accept();
 
-            await _repository.Upsert(quote).ConfigureAwait(false);
-            
-            throw new Exception("Something went wrong.");
+            _repository.Upsert(quote);
 
             await context.Publish(new QuoteAcceptedEvent(message.QuoteNumber)).ConfigureAwait(false);
         }
